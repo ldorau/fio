253fce75cfc0a9e32d0d1cace21b428ca6079454
Lukasz Dorau <lukasz.dorau@intel.com>
2020-10-22 14:34:46 +0200

    rpma: MY

---
+++ cgdb.cmd
@@ -0,0 +1,17 @@
+b server_init
+b server_open_file
+b server_close_file
+b server_queue
+b server_cleanup
+
+b client_init
+b client_post_init
+b client_setup
+b client_open_file
+b client_queue
+b client_commit
+b client_getevents
+b client_event
+b client_errdetails
+b client_close_file
+b client_cleanup
